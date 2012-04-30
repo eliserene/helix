b5b065027315d6783e242e3b8e52a8b7bd5d64e1
Elise Dettrick <elise@dettrick.net>
2012-04-30 14:08:21 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helix::Application.config.secret_token = '8e14c0a88d202cb48284caa793522cd546b0d68b314a70570679fc95e8b8061df2d852eee80ffbbc70fd27d342fbf27cf425dbb9f39a1590a56adf05322cfe75'
